461891afdf03fd11dfaf82d36a0bae71a33cf5b2
kgreenoita-droid <kgreenoita@gmail.com>
2026-07-12 15:19:28 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+/* このファイルにだけ、GAS ウェブアプリの /exec URL を記載します。
+   index.html を差し替えても、このファイルは差し替えなければ URL はそのまま使われます。
+   URL が変わったときだけ、下の "" の中身を書き換えてください。 */
+window.APP_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbx0L7Uy7SLvhD3Ch9IWI9fWMpF4eZtMTpoaKbB-8aYYxbaxxRb1SLudCeUeDwy3w-Z2/exec"};
